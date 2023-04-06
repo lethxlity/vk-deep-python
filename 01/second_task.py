6684179ce50983bda3913dd5e3a7b01962c2fa1b
lethxlity <50768476+lethxlity@users.noreply.github.com>
2023-04-06 21:23:44 +0300

    Delete 01 directory

--- 01/second_task.py
+++
@@ -1,6 +0,0 @@
-def gen(filename: str, words: list):
-    with open(filename, "r", encoding="utf8") as text_file:
-        for string in text_file:
-            for word in words:
-                if word.lower() in string.lower():
-                    yield string

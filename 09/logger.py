7284928e557e3d1dfa6859db19a06f5a9fb5d0ce
lethxlity <50768476+lethxlity@users.noreply.github.com>
2023-05-28 17:39:30 +0300

    Add files via upload

--- 09/logger.py
+++ 09/logger.py
@@ -22,26 +22,10 @@ class CustomFilter(logging.Filter):
 
 
 class LRUCache:
-    file_handler = logging.FileHandler(filename="cache.log",
-                                       mode="w",
-                                       encoding="utf-8")
-    stream_handler = logging.StreamHandler(stream=sys.stdout)
-
-    def __init__(self, limit=42, sout_logging=False, filt=False):
-        if filt:
-            LRUCache.file_handler.addFilter(CustomFilter())
-            LRUCache.stream_handler.addFilter(CustomFilter())
-        else:
-            LRUCache.file_handler.removeFilter(CustomFilter())
-            LRUCache.stream_handler.removeFilter(CustomFilter())
-        handlers = [LRUCache.file_handler]
-        if sout_logging:
-            handlers.append(LRUCache.stream_handler)
-        logging.basicConfig(level=logging.DEBUG,
-                            format="%(asctime)s %(levelname)s %(message)s",
-                            handlers=handlers)
+    def __init__(self, logger_name, limit=42):
+        self.log = logging.getLogger(logger_name)
         if limit <= 0:
-            logging.error(f"raised creating cache, "
+            self.log.error(f"raised creating cache, "
                           f"limit must be > 0,"
                           f" current limit value: {limit}")
             raise ValueError
@@ -52,16 +36,16 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
 
-        logging.debug(f"Cache created, size: {limit}")
+        self.log.debug(f"Cache created, size: {limit}")
 
     def get(self, key):
         if key in self.dict:
             node = self.dict[key]
             self.remove_from_list(node)
             self.insert_to_head(node)
-            logging.info(f"GET value: {node.val}, key: {key}, SUCCESS")
+            self.log.info(f"GET value: {node.val}, key: {key}, SUCCESS")
             return node.val
-        logging.info(f"GET value: {None}, key: {key}, NO KEY")
+        self.log.info(f"GET value: {None}, key: {key}, NO KEY")
         return None
 
     def set(self, key, val):
@@ -70,16 +54,16 @@ class LRUCache:
             self.remove_from_list(node)
             self.insert_to_head(node)
             node.val = val
-            logging.info(f"SET value: {val}, EXISTING key: {key}, SUCCESS")
+            self.log.info(f"SET value: {val}, EXISTING key: {key}, SUCCESS")
 
         else:
             if len(self.dict) >= self.limit:
-                logging.info(f"--------CAPACITY EXCEEDED--------")
+                self.log.info(f"--------CAPACITY EXCEEDED--------")
                 self.remove_from_tail()
             node = ListNode(key, val)
             self.dict[key] = node
             self.insert_to_head(node)
-        logging.info(f"SET value: {val}, NEW key: {key}, SUCCESS")
+        self.log.info(f"SET value: {val}, NEW key: {key}, SUCCESS")
 
     def remove_from_list(self, node: ListNode):
         node.prev.next = node.next
@@ -109,7 +93,27 @@ if __name__ == "__main__":
             sout_logging = True
         elif opt == "-f":
             filt = True
-    cache = LRUCache(2, sout_logging, filt)
+
+    logger = logging.getLogger('my_logger')
+    formatter1 = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    formatter2 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler(filename="cache.log",
+                                       mode="w",
+                                       encoding="utf-8")
+    file_handler.setFormatter(formatter1)
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setFormatter(formatter2)
+
+    if filt:
+        file_handler.addFilter(CustomFilter())
+        stream_handler.addFilter(CustomFilter())
+    handlers = [file_handler]
+    if sout_logging:
+        handlers.append(stream_handler)
+    logging.basicConfig(level=logging.DEBUG,
+                        handlers=handlers)
+
+    cache = LRUCache('my_logger', 2)
     cache.set("k1", "aaa")
     cache.set("k2", "val2")
     cache.set("k3", "val3")
@@ -118,6 +122,6 @@ if __name__ == "__main__":
     cache.get("k2")
     cache.get("k3")
     try:
-        cache2 = LRUCache(-1)
+        cache2 = LRUCache('my_logger', -1)
     except ValueError:
         pass
